1785eb1bb4fa1eb230351ab6a0d133403b82142a
Sankara <sankara.ishema@gmail.com>
2017-03-20 18:30:24 +0100

    First usage of BoxLayout in positioning buttons.

--- src/Window.java
+++ src/Window.java
@@ -1,13 +1,13 @@
 import java.awt.BorderLayout;
 import java.awt.GridLayout;
 
+import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
 public class Window extends JFrame{
 	
-	private JButton button = new JButton("My Button");
 	
 	public Window() {
 		this.setTitle("Interesting Window");
@@ -15,15 +15,28 @@ public class Window extends JFrame{
 		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
 		this.setLocationRelativeTo(null);
 		
-		this.setLayout(new GridLayout(3,2));
+		JPanel boxPanel1 = new JPanel();
+		boxPanel1.setLayout(new BoxLayout(boxPanel1, BoxLayout.LINE_AXIS));
+		boxPanel1.add(new JButton("1"));
 		
-		this.getContentPane().add(new JButton("1"));
-		this.getContentPane().add(new JButton("2"));
-		this.getContentPane().add(new JButton("3"));
-		this.getContentPane().add(new JButton("4"));
-		this.getContentPane().add(new JButton("5"));
-		this.getContentPane().add(new JButton("6"));
+		JPanel boxPanel2 = new JPanel();
+		boxPanel2.setLayout(new BoxLayout(boxPanel2, BoxLayout.LINE_AXIS));
+		boxPanel2.add(new JButton("2"));
+		boxPanel2.add(new JButton("3"));
 		
+		JPanel boxPanel3 = new JPanel();
+		boxPanel3.setLayout(new BoxLayout(boxPanel3, BoxLayout.LINE_AXIS));
+		boxPanel3.add(new JButton("4"));
+		boxPanel3.add(new JButton("5"));
+		boxPanel3.add(new JButton("6"));
+		
+		JPanel boxPanel4 = new JPanel();
+		boxPanel3.setLayout(new BoxLayout(boxPanel4, BoxLayout.PAGE_AXIS));
+		boxPanel4.add(boxPanel1);
+		boxPanel4.add(boxPanel2);
+		boxPanel4.add(boxPanel3);
+		
+		this.getContentPane().add(boxPanel4);
 		this.setVisible(true);
 	}
 }
