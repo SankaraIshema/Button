606f14c7dc6fa99d88ac20a86d6954f2a8ca51be
Sankara <sankara.ishema@gmail.com>
2017-03-21 18:22:16 +0100

    GridBagLayout with commentaries. Because Comments Lives Matters

--- src/Window.java
+++ src/Window.java
@@ -21,84 +21,136 @@ public class Window extends JFrame{
 		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
 		this.setLocationRelativeTo(null);
 		
-		JPanel casePanel1 = new JPanel();
-		casePanel1.setBackground(Color.YELLOW);
-		casePanel1.setPreferredSize(new Dimension(60, 40));
+		/* We have 8 colours to set up for our Mondrian.
+		 * Each colour will have it's own panel and
+		 * each panel will have the same dimensions
+		 */
+		JPanel colorPanel1 = new JPanel();
+		colorPanel1.setBackground(Color.YELLOW);
+		colorPanel1.setPreferredSize(new Dimension(60, 40));
 		
-		JPanel casePanel2 = new JPanel();
-		casePanel2.setBackground(Color.RED);
-		casePanel2.setPreferredSize(new Dimension(60, 40));
+		JPanel colorPanel2 = new JPanel();
+		colorPanel2.setBackground(Color.RED);
+		colorPanel2.setPreferredSize(new Dimension(60, 40));
 		
-		JPanel casePanel3 = new JPanel();
-		casePanel3.setBackground(Color.GREEN);
-		casePanel3.setPreferredSize(new Dimension(60, 40));
+		JPanel colorPanel3 = new JPanel();
+		colorPanel3.setBackground(Color.GREEN);
+		colorPanel3.setPreferredSize(new Dimension(60, 40));
 		
-		JPanel casePanel4 = new JPanel();
-		casePanel4.setBackground(Color.BLACK);
-		casePanel4.setPreferredSize(new Dimension(60, 40));
+		JPanel colorPanel4 = new JPanel();
+		colorPanel4.setBackground(Color.BLACK);
+		colorPanel4.setPreferredSize(new Dimension(60, 40));
 		
-		JPanel casePanel5 = new JPanel();
-		casePanel5.setBackground(Color.CYAN);
-		casePanel5.setPreferredSize(new Dimension(60, 40));
+		JPanel colorPanel5 = new JPanel();
+		colorPanel5.setBackground(Color.CYAN);
+		colorPanel5.setPreferredSize(new Dimension(60, 40));
 		
-		JPanel casePanel6 = new JPanel();
-		casePanel6.setBackground(Color.BLUE);
-		casePanel6.setPreferredSize(new Dimension(60, 40));
+		JPanel colorPanel6 = new JPanel();
+		colorPanel6.setBackground(Color.BLUE);
+		colorPanel6.setPreferredSize(new Dimension(60, 40));
 		
-		JPanel casePanel7 = new JPanel();
-		casePanel7.setBackground(Color.ORANGE);
-		casePanel7.setPreferredSize(new Dimension(60, 40));
+		JPanel colorPanel7 = new JPanel();
+		colorPanel7.setBackground(Color.ORANGE);
+		colorPanel7.setPreferredSize(new Dimension(60, 40));
 		
-		JPanel casePanel8 = new JPanel();
-		casePanel8.setBackground(Color.DARK_GRAY);
-		casePanel8.setPreferredSize(new Dimension(60, 40));
+		JPanel colorPanel8 = new JPanel();
+		colorPanel8.setBackground(Color.DARK_GRAY);
+		colorPanel8.setPreferredSize(new Dimension(60, 40));
 		
+		// We create the mother panel that will receive the colour panels
 		JPanel motherPane = new JPanel();
 		motherPane.setBackground(Color.WHITE);
 		motherPane.setPreferredSize(new Dimension(300, 120));
 		
+		/* We set the GridBagLaout for our mother panel
+		 * and we create an instance of GridBagConstraints that will handle the colour panels
+		 */
 		motherPane.setLayout(new GridBagLayout());
 		GridBagConstraints gridBagConstraints = new GridBagConstraints();
 		
+		// LET'S CREATE OUR PIECE OF ART
+		
+		/* We specify to the gridBagConstraints where to start
+		 * In our case, the upper left corner of our mother pane.
+		 */
 		gridBagConstraints.gridx = 0;
 		gridBagConstraints.gridy = 0;
+		
+		//We specify the width and height we want for our colour panel
 		gridBagConstraints.gridwidth = 1;
 		gridBagConstraints.gridheight = 1;
-		motherPane.add(casePanel1, gridBagConstraints);
 		
+		/* Then we add the colour panel - with it's gridBagConstraints containing
+		 * all it's specifications - to the mother panel
+		 */
+		motherPane.add(colorPanel1, gridBagConstraints);
+		
+		//---------------------------------------------
+		
+		/* For placing our second colour panel it is important to remember
+		 * that our specifications - gridx, gridy, gridwidth and gridheight -
+		 * are still stored. So we only need to change the specificity we need changing.
+		 * In this case, we start 1 step to the right from our previous position,
+		 * at the same level, with the same width and height as before. 
+		 * Thus, only gridx has to change
+		 */
 		gridBagConstraints.gridx = 1;
-		motherPane.add(casePanel2, gridBagConstraints);
+		motherPane.add(colorPanel2, gridBagConstraints);
+		
+		//---------------------------------------------
 		
 		gridBagConstraints.gridx = 2;
-		motherPane.add(casePanel3, gridBagConstraints);
+		motherPane.add(colorPanel3, gridBagConstraints);
 		
+		//---------------------------------------------
+		
+		// We must here warn our gridBagConstraints that it has reached the end of the line
 		gridBagConstraints.gridwidth = GridBagConstraints.REMAINDER;
 		gridBagConstraints.gridx = 3;
-		motherPane.add(casePanel4, gridBagConstraints);
+		motherPane.add(colorPanel4, gridBagConstraints);
+		
+		//---------------------------------------------
 		
 		gridBagConstraints.gridx = 0;
 		gridBagConstraints.gridy = 1;
 		gridBagConstraints.gridwidth = 1;
 		gridBagConstraints.gridheight = 2;
+		
+		// We must warn our gridBagConstraints that this one replicate itself vertically
 		gridBagConstraints.fill = GridBagConstraints.VERTICAL;
-		motherPane.add(casePanel5, gridBagConstraints);
+		motherPane.add(colorPanel5, gridBagConstraints);
+		
+		//---------------------------------------------
 		
 		gridBagConstraints.gridx = 1;
 		gridBagConstraints.gridheight = 1;
+		
+		// We must warn our gridBagConstraints that this replicate itself horizontally
 		gridBagConstraints.fill = GridBagConstraints.HORIZONTAL;
 		gridBagConstraints.gridwidth = GridBagConstraints.REMAINDER;
-		motherPane.add(casePanel6, gridBagConstraints);
+		motherPane.add(colorPanel6, gridBagConstraints);
 		
+		//---------------------------------------------
+
 		gridBagConstraints.gridx = 1;
 		gridBagConstraints.gridy = 2;
 		gridBagConstraints.gridwidth = 2;
-		motherPane.add(casePanel7, gridBagConstraints);
+		motherPane.add(colorPanel7, gridBagConstraints);
 		
+		//---------------------------------------------
+
 		gridBagConstraints.gridx = 3;
 		gridBagConstraints.gridwidth = GridBagConstraints.REMAINDER;
-		motherPane.add(casePanel8, gridBagConstraints);
+		motherPane.add(colorPanel8, gridBagConstraints);
 
+		//---------------------------------------------
+		
+		// We set the mother panel as the main panel of our window
 		this.setContentPane(motherPane);
+		
+		/* We then show the true definition of Shock and Awe that is our creation
+		 * for all the world to see
+		 */
 		this.setVisible(true);
 	}
 }
