4f3a297ff78af19f8cf37dfea85ad3636bbe071b
Sankara <sankara.ishema@gmail.com>
2017-03-21 10:30:37 +0100

    First successful GridBagLayout experiment

--- src/Window.java
+++ src/Window.java
@@ -1,6 +1,9 @@
 import java.awt.BorderLayout;
 import java.awt.CardLayout;
 import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -12,78 +15,90 @@ import javax.swing.JPanel;
 
 public class Window extends JFrame{
 	
-	// Our Layout
-	CardLayout cardLayout = new CardLayout(); 
-	
-	// The panel that will receive all the other panels
-	JPanel motherPane = new JPanel(); 
-	
-	/* List of all the daughter panels that will be receive by the mother-panel
-	 * This list will be used by the second method of shifting through panels that we will try today*/
-	String[] listDaughterPane = {"DAUGHTER_PANE_1", "DAUGHTER_PANE_2", "DAUGHTER_PANE_3"};
-	
-	// integer needed for our second method
-	int i = 0; 
-	
 	public Window() {
 		this.setTitle("Interesting Window");
-		this.setSize(300, 150);
+		this.setSize(300, 160);
 		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
 		this.setLocationRelativeTo(null);
 		
-		// The three panel we will use with their distinct background colour
-		JPanel daughterPane1 = new JPanel();
-		daughterPane1.setBackground(Color.BLUE);
-		JPanel daughterPane2 = new JPanel();
-		daughterPane2.setBackground(Color.YELLOW);
-		JPanel daughterPane3 = new JPanel();
-		daughterPane3.setBackground(Color.RED);	
+		JPanel casePanel1 = new JPanel();
+		casePanel1.setBackground(Color.YELLOW);
+		casePanel1.setPreferredSize(new Dimension(60, 40));
 		
-		// First button with it's action listener ready to implements method 1
-		JButton button1 = new JButton("Next Content");
-		button1.addActionListener(new ActionListener() {
-			
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				cardLayout.next(motherPane);
-				
-			}
-		});
+		JPanel casePanel2 = new JPanel();
+		casePanel2.setBackground(Color.RED);
+		casePanel2.setPreferredSize(new Dimension(60, 40));
 		
-		// Second button with it's action listener ready to implements method 2
-		JButton button2 = new JButton("Content by Indice");
-		button2.addActionListener(new ActionListener() {
-			
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				if(++i > 2) i = 0;
-				cardLayout.show(motherPane, listDaughterPane[i]);
-				
-			}
-		});
+		JPanel casePanel3 = new JPanel();
+		casePanel3.setBackground(Color.GREEN);
+		casePanel3.setPreferredSize(new Dimension(60, 40));
 		
-		// We collect our buttons into one panel
-		JPanel buttonPane = new JPanel();
-		buttonPane.add(button1);
-		buttonPane.add(button2);
+		JPanel casePanel4 = new JPanel();
+		casePanel4.setBackground(Color.BLACK);
+		casePanel4.setPreferredSize(new Dimension(60, 40));
 		
-		/* We set the CardLayout into our main panel 
-		 * and then we add the three daughter panels to the mother panel. */
-		motherPane.setLayout(cardLayout);
-		motherPane.add(daughterPane1, listDaughterPane[0]);
-		motherPane.add(daughterPane2, listDaughterPane[1]);
-		motherPane.add(daughterPane3, listDaughterPane[2]);
+		JPanel casePanel5 = new JPanel();
+		casePanel5.setBackground(Color.CYAN);
+		casePanel5.setPreferredSize(new Dimension(60, 40));
 		
-		/* We then ask our window to ask it's main panel to add our button panel
-		 * into the upward region of the main panel */
-		this.getContentPane().add(buttonPane, BorderLayout.NORTH);
+		JPanel casePanel6 = new JPanel();
+		casePanel6.setBackground(Color.BLUE);
+		casePanel6.setPreferredSize(new Dimension(60, 40));
 		
-		/* Then we ask our window to ask it's main panel to add our mother panel
-		 * into the middle region of  the main panel */
-		this.getContentPane().add(motherPane, BorderLayout.CENTER);
+		JPanel casePanel7 = new JPanel();
+		casePanel7.setBackground(Color.ORANGE);
+		casePanel7.setPreferredSize(new Dimension(60, 40));
 		
-		/* And at last, we set the window visible so the people 
-		 * may feast it's eyes on the marvel you've created */
+		JPanel casePanel8 = new JPanel();
+		casePanel8.setBackground(Color.DARK_GRAY);
+		casePanel8.setPreferredSize(new Dimension(60, 40));
+		
+		JPanel motherPane = new JPanel();
+		motherPane.setBackground(Color.WHITE);
+		motherPane.setPreferredSize(new Dimension(300, 120));
+		
+		motherPane.setLayout(new GridBagLayout());
+		GridBagConstraints gridBagConstraints = new GridBagConstraints();
+		
+		gridBagConstraints.gridx = 0;
+		gridBagConstraints.gridy = 0;
+		gridBagConstraints.gridwidth = 1;
+		gridBagConstraints.gridheight = 1;
+		motherPane.add(casePanel1, gridBagConstraints);
+		
+		gridBagConstraints.gridx = 1;
+		motherPane.add(casePanel2, gridBagConstraints);
+		
+		gridBagConstraints.gridx = 2;
+		motherPane.add(casePanel3, gridBagConstraints);
+		
+		gridBagConstraints.gridwidth = GridBagConstraints.REMAINDER;
+		gridBagConstraints.gridx = 3;
+		motherPane.add(casePanel4, gridBagConstraints);
+		
+		gridBagConstraints.gridx = 0;
+		gridBagConstraints.gridy = 1;
+		gridBagConstraints.gridwidth = 1;
+		gridBagConstraints.gridheight = 2;
+		gridBagConstraints.fill = GridBagConstraints.VERTICAL;
+		motherPane.add(casePanel5, gridBagConstraints);
+		
+		gridBagConstraints.gridx = 1;
+		gridBagConstraints.gridheight = 1;
+		gridBagConstraints.fill = GridBagConstraints.HORIZONTAL;
+		gridBagConstraints.gridwidth = GridBagConstraints.REMAINDER;
+		motherPane.add(casePanel6, gridBagConstraints);
+		
+		gridBagConstraints.gridx = 1;
+		gridBagConstraints.gridy = 2;
+		gridBagConstraints.gridwidth = 2;
+		motherPane.add(casePanel7, gridBagConstraints);
+		
+		gridBagConstraints.gridx = 3;
+		gridBagConstraints.gridwidth = GridBagConstraints.REMAINDER;
+		motherPane.add(casePanel8, gridBagConstraints);
+
+		this.setContentPane(motherPane);
 		this.setVisible(true);
 	}
 }
